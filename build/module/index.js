9b3691c1caa7cfb6e9305441b291e7671730475f
Rizky <rizky@andromedia.co.id>
2020-01-03 17:24:28 +0700

    fix

---
+++ build/module/index.js
@@ -0,0 +1,2 @@
+export const useNavigation = {};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsTUFBTSxDQUFDLE1BQU0sYUFBYSxHQUFHLEVBQVMsQ0FBQyJ9
